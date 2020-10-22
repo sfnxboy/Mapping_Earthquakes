ede5202525ed71fa4f52d525cb3a8f586a50a799
sfnxboy <amiramire12@gmail.com>
2020-10-21 23:39:27 -0400

    updating logic.js file

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30,30], 2);
+//let map = L.map('mapid').setView([30,30], 2);
 // setView method sets the coordinates and the level of zoom
 
 
@@ -21,8 +21,30 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Then we add our 'streets' tile layer to the map.
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
 streets.addTo(map);
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 
 /* To change the map's style, change the map id using the list of Mapbox ids below:
