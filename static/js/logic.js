db7877731bd879bf533a488fcdebf6ee52e5f1b7
sfnxboy <amiramire12@gmail.com>
2020-10-22 11:05:49 -0400

    Updating logic.js file to include Toronto Airline routes

--- static/js/logic.js
+++ static/js/logic.js
@@ -28,23 +28,31 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
     accessToken: API_KEY
 });
 
+let light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+
+})
+
 // Create a base layer that holds both maps.
 let baseMaps = {
   Street: streets,
-  Dark: dark
+  Dark: dark,
+  light: light
 };
 // Then we add our 'streets' tile layer to the map.
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
   layers: [streets]
 })
 streets.addTo(map);
 
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps).addTo(map); 
 
 
 /* To change the map's style, change the map id using the list of Mapbox ids below:
@@ -203,7 +211,7 @@ L.geoJson(sanFranAirport, {
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/sfnxboy/Mapping_Earthquakes/main/majorAirports.json"
 
-// Grabbing our GeoJSON data.
+/*
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
@@ -213,6 +221,27 @@ L.geoJson(data, {
     console.log(feature);
     return L.marker(latlng)
     .bindPopup("<h2>" + feature.properties.faa + "</h2> <hr> <h3>" +  feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+});*/
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/sfnxboy/Mapping_Earthquakes/main/torontoRoutes.json";
+
+let myStyle = {
+  color: "yellow",
+  weight: 3
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    console.log(feature);
+    layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h3> Destination: " + feature.properties.dst + "</h3>")
   }
 }).addTo(map);
 });
