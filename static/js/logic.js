8f15d7fc6f83d4faf6b8c493e900b7b9bc91da5b
sfnxboy <amiramire12@gmail.com>
2020-10-22 13:55:02 -0400

    Updating logic.js to contain all earthquake data for the last 7 days

--- static/js/logic.js
+++ static/js/logic.js
@@ -51,8 +51,8 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [satelliteStreets]
 })
 //satelliteStreets.addTo(map);
@@ -264,4 +264,10 @@ d3.json(torontoHoods).then(function(data) {
       layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>")
     }
   }).addTo(map);
+});
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
 });
